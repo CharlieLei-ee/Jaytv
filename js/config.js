dd5d5809423957baf30f0d673a904e444fa1b1af
CharlieLei-ee <127936394+CharlieLei-ee@users.noreply.github.com>
2025-07-09 14:26:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'NiuTV',
-    url: 'https://niutv.is-an.org',
+    name: 'Jay-TV',
+    url: 'https://Jay-TV.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
